332552ac005a52a3dd392521b7d6a00499d00d60
Francesco Pizzo <francesco.pizzo@sytac.io>
2018-04-23 19:33:20 +0200

    fixed double jsonization

---
+++ streamer/src/main/java/nl/sytac/intro/microservices/streamer/TweetsWrapper.java
@@ -0,0 +1,15 @@
+package nl.sytac.intro.microservices.streamer;
+
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class TweetsWrapper {
+
+    @JsonRawValue
+    List<String> tweets;
+}
--- streamer/src/main/java/nl/sytac/intro/microservices/streamer/TwitterEndpoint.java
+++ streamer/src/main/java/nl/sytac/intro/microservices/streamer/TwitterEndpoint.java
@@ -1,9 +1,7 @@
 package nl.sytac.intro.microservices.streamer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +15,12 @@ public class TwitterEndpoint {
     private HoseBirdService hoseBirdService;
 
     @GetMapping
-    public ResponseEntity giveMeTweets(
+    public TweetsWrapper giveMeTweets(
                 @RequestParam(value = "hashtag") String hashTag,
                 @RequestParam(value = "max") Integer max,
                 @RequestParam(value = "timeout") Integer timeout
             ) throws InterruptedException {
 
-//        List<String> asd = Arrays.asList("{\"created_at\":\"Mon Apr 23 16:51:20 +0000 2018\",\"id\":988460321902915585}");
-//        return asd;
-        return new ResponseEntity<>(hoseBirdService.giveMeTweets(hashTag, max, timeout), HttpStatus.OK);
-//        return hoseBirdService.giveMeTweets(hashTag, max, timeout);
+        return new TweetsWrapper(hoseBirdService.giveMeTweets(hashTag, max, timeout));
     }
 }
