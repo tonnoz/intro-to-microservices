704c7be237634cc77138b087e29b134775f08781
justin <j.seyvecou@tmg.nl>
2018-04-23 14:49:24 +0200

    Integration with Babel translate to ES7.

--- registry/registry.js
+++ registry/registry.js
@@ -1,8 +1,8 @@
-var express = require("express");  
-var rest = require('unirest');
-var app = express();  
+import express from "express"
+import fetch from 'node-fetch'
 
-var registry = {}
+const app = express()
+const registry = {}
 
 app.post("/:name/:port", function(request, response) {  
     var name = request.params.name
@@ -16,7 +16,7 @@ app.post("/:name/:port", function(request, response) {  
     if (ports.indexOf(port) == -1) {
         ports.push(port)
         registry[name] = ports
-        console.log("Registered service "+name+" at port "+port+" - registry: "+JSON.stringify(registry));
+        console.log(`Registered service ${name} at port ${port} - registry: ${JSON.stringify(registry)}`)
     }
 
     response.status(201);
@@ -30,46 +30,45 @@ app.get("/:name", function(request, response) {  
     var name = request.params.name
     var ports = registry[name]
     if (ports === undefined) {
-        response.status(404);
+        response.status(404)
         response.json({
             "name": name,
-            "error": "Service " + name + " not found!"
-        });
-        return;
+            "error": `Service ${name} not found!`
+        })
+        return
     }   
 
     response.json({
         "name": name,
         "port": ports[0]
-    });
-});
+    })
+})
 
 app.get("/", function(request, response) {  
-    text = JSON.stringify(registry);
-    response.write(text);
-    response.end();
-});
+    text = JSON.stringify(registry)
+    response.write(text)
+    response.end()
+})
 
 app.listen(3000, function() {                       
-    console.log("Registry service started on port 3000");
-});
+    console.log("Registry service started on port 3000")
+})
 
-setInterval(function() {
-
-    function check(name, port) {
-        var url = "http://localhost:" + port + "/ping";
-        rest.get(url).end(function(res) {
+setInterval(() => {
+    const check = (name, port) => {
+        const url = "http://localhost:" + port + "/ping"
+        fetch(url).then(function(res) {
             if (res.error) {
                 registry[name] = ports.splice(index, 1)
                 console.log("Service '" + name + "' at port " + port + " is NOT working - unregistering it!!!")
             }
-        });
+        })
     }
 
-    for (var name in registry) {
-        var ports = registry[name];
-        for (var index = 0; index < ports.length; index++) {
-            check(name, ports[index]);
+    for (let name in registry) {
+        const ports = registry[name]
+        for (let index = 0; index < ports.length; index++) {
+            check(name, ports[index])
         }
     }
-}, 10000);
+}, 10000)
