5eba4234d909287b03c7e496873ddf273ccbcf4e
Tony Catapano <tonycatap@gmail.com>
2018-05-03 22:58:06 +0200

    redis in progress

--- nodejs-express-service-edge/edge.js
+++ nodejs-express-service-edge/edge.js
@@ -5,9 +5,12 @@ const app = express();
 const RUNNING_ON_PORT = 3001;
 const SERVICES_POLLING_INTERVAL_MSEC = 5000;
 const REGISTRY_PORT = 3000;
+const redis = require('redis')
+
+// Create a standart redis client which will be a reader in our case.
+const redCli = redis.createClient();
 
 var port_time; //the dynamically assigned port for the time service
-var port_rand; //the dynamically assigned port for the rand service
 
 /** function to return safely the object data[name] from the data object **/
 const uparse = function(data, name) {
@@ -15,7 +18,8 @@ const uparse = function(data, name) {
 }
 
 
-/** GET endpoint for the Edge service: call in parallel the rand and time service and compose the return response **/
+
+/** GET endpoint for the Edge service: call in parallel the tweets and time service and compose the return response **/
 app.get("/", function(request, response) { 
     console.log("Request received from " + request.ip);
 
@@ -30,22 +34,18 @@ app.get("/", function(request, response) {
                 callback(null, null);
             }
         },
-        rand: function(callback) {
-            if(port_rand) { //call the service only if the port is available (have been communicated from the registry)
-                rest.get("http://localhost:" + port_rand).end(function (res) {
-                    callback(null, res.body);
-                });
-            }else{
-                console.warn("I was not able to call the rand service since I am not aware of its running port");
-                callback(null, null);
-            }
+        tweets: function(callback) {
+            redCli.get("tweets", function(err, reply) {
+                console.log(reply);  // reply is null when the key is missing
+                callback(null, reply);
+            });
         }
     },
     function(err, results) {
         if (!err) {
             const message = "Hello stranger!" +
                 (results.time ? "\n- today is " + uparse(results.time, "time") : "" ) +
-                (results.rand ? "\n- your lucky number is " + uparse(results.rand, "number") : "")+"\n";
+                (results.tweets ? "tweets:"+ results.tweets : "")+"\n";
             response.send(message);
         } else {
             console.log(err);
@@ -66,11 +66,6 @@ setInterval(function() {
     rest.get("http://localhost:"+ REGISTRY_PORT + "/time/").end(function(res) {
         port_time = res.body;
         console.log("registry replied me that the 'time service' is running at port : "+ port_time);
-    });
-
-    rest.get("http://localhost:"+ REGISTRY_PORT + "/rand/").end(function(res) {
-        port_rand = res.body;
-        console.log("registry replied me that the 'rand service' is running at port : "+ port_rand);
     });
 
 }, SERVICES_POLLING_INTERVAL_MSEC);
